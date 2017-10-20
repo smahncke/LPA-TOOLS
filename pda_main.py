60cb69c2204ad48b316d6d27b4db94a543326683
SMahncke <sebastian.mahncke@desy.de>
2017-10-20 16:47:50 +0200

    Bugfixes
    No diag/hdf5 files are needed anymore, so this script can bes started without a FBPIC simulation before.
    The number of iterations can be set manually aswell, so the plots are a way smoother

--- pda_main.py
+++ pda_main.py
@@ -1,22 +1,15 @@
 
 ### IMPORTS CLASSES
 import numpy as np
-import csv
 import math
-import time
 import os
-import sys
 import matplotlib.pyplot as plt
 from pylab import title, show
-from opmd_viewer.addons.pic import LpaDiagnostics as OpenPMDTimeSeries
 from scipy.constants import c
-
-# Global settings
-ts = OpenPMDTimeSeries('./diags/hdf5/') #The simulation files which should be imported
 
 # Gas settings
 n_tot = 1e18*1e6 #Total Plasma Density
-am_N2 = 20 #Amount of nitrogen (in percent)
+am_N2 = 100 #Amount of nitrogen (in percent)
 
 # Density profiles
 FWHM = 300e-6 #Full-width at half-max of the gaussian density profile (in m)
@@ -26,6 +19,8 @@ plateau = 4000e-6 #The length of the plateau (in m)
 
 # Other settings
 inj_thres = 6 #The ionization level of N2, at which the electron injection starts
+
+iterations = 10000 #Number of iterations (higher values result in a smoother plot, minimum should be 1000)
 
 ####################################################################################
 # - Dont change anything below this line
@@ -43,7 +38,8 @@ r=1
 am_H2 = 100 - am_N2 #Amount of hydrogen
 
 # Initialization of the density arrays
-a0_ts = np.zeros_like(ts.t, dtype=object)
+z_array = np.linspace(0,100,num=iterations*1.01)
+a0_ts = np.zeros_like(z_array, dtype=object)
 gas_dens_H2 = a0_ts.copy()
 gas_dens_N2 = a0_ts.copy()
 unknown_dens = a0_ts.copy()
@@ -56,28 +52,28 @@ gas_dens_tot = a0_ts.copy()
 e_dens_tot_test = a0_ts.copy()
 gas_dens_H2_test = a0_ts.copy()
 
-
+dividor = 2e4*(iterations/100)
 ### Calculating the density profiles
 
-for ii, t in enumerate(ts.t):
+for ii,t in enumerate(z_array):
         
     # Density of the main plasma
-    if (t*c) < ramp_start:
+    if (ii/dividor) < ramp_start:
         e_dens_tot[ii] = 0
-    elif (t*c) < (ramp_start+ramp_length):
-        e_dens_tot[ii] = n_tot*np.sin((0.5*np.pi*(t*c-ramp_start))/(ramp_length))**2
-    elif (ramp_start + 2*ramp_length + plateau)>(t*c)>(ramp_start+ramp_length + plateau):
-        e_dens_tot[ii] = n_tot*np.sin((0.5*np.pi*(t*c-ramp_start))/ramp_length)**2
-    elif (t*c) >= (ramp_start + 2*ramp_length + plateau):
+    elif (ii/dividor) < (ramp_start+ramp_length):
+        e_dens_tot[ii] = n_tot*np.sin((0.5*np.pi*((ii/dividor)-ramp_start))/(ramp_length))**2
+    elif (ramp_start + 2*ramp_length + plateau)>(ii/dividor)>(ramp_start+ramp_length + plateau):
+        e_dens_tot[ii] = n_tot*np.sin((0.5*np.pi*((ii/dividor)-ramp_start))/ramp_length)**2
+    elif (ii/dividor) >= (ramp_start + 2*ramp_length + plateau):
         e_dens_tot[ii] = 0
     else:
         e_dens_tot[ii] = n_tot
 		
     #???
-    unknown_dens[ii] = 0.5*e_dens_tot[ii]*(1/(1-(am_N2/100 -(inj_thres-1)*(am_N2/100))*np.exp(-((c*t)-mu)**2/(2*sigma**2))))
+    unknown_dens[ii] = 0.5*e_dens_tot[ii]*(1/(1-(am_N2/100 -(inj_thres-1)*(am_N2/100))*np.exp(-((ii/dividor)-mu)**2/(2*sigma**2))))
         
     # Gas density of the nitrogen
-    gas_dens_N2[ii] = (unknown_dens[ii]-0.5*e_dens_tot[ii])*(1/(2-inj_thres))*np.exp(-((c*t)-mu)**2/(2*sigma**2)) 
+    gas_dens_N2[ii] = (unknown_dens[ii]-0.5*e_dens_tot[ii])*(1/(2-inj_thres))*np.exp(-((ii/dividor)-mu)**2/(2*sigma**2)) 
         
     # Electron density of the nitrogen
     e_dens_N2[ii] = 2*gas_dens_N2[ii]*(inj_thres-1)
@@ -94,6 +90,7 @@ for ii, t in enumerate(ts.t):
     # Control functions
     e_dens_tot_test[ii] = 2*gas_dens_H2[ii] + 2*(inj_thres-1)*gas_dens_N2[ii]
     gas_dens_H2_test[ii] = (e_dens_tot[ii] - 2*(inj_thres-1)*gas_dens_N2[ii])/2 
+    
         
 
 # Save the arrays as a .dat file
@@ -120,10 +117,10 @@ ax.grid(which='major', alpha=0.8, color='grey', linestyle='--', linewidth=0.6, a
 ax.grid(which='minor', alpha=0.6, color='grey', linestyle='--', linewidth=0.5, animated='True')
 
 # Make the plots
-ax.plot(ts.t*c*1e3, e_dens_tot, linestyle='--' ,color='grey', label="$n_e (tot)$")
-ax.plot(ts.t*c*1e3, gas_dens_tot, linestyle='--' ,color='red', label="$n_{gas} (tot)$")
-ax.plot(ts.t*c*1e3, gas_dens_H2 ,color='#31B5D6', label="$n_{gas} (H2)$")
-ax.plot(ts.t*c*1e3, gas_dens_N2, linestyle='--', color='#7BC618', label="$n_{gas} (N2)$")
+ax.plot(z_array/20, e_dens_tot, linestyle='--' ,color='grey', label="$n_e (tot)$")
+ax.plot(z_array/20, gas_dens_tot, linestyle='--' ,color='red', label="$n_{gas} (tot)$")
+ax.plot(z_array/20, gas_dens_H2 ,color='#31B5D6', label="$n_{gas} (H2)$")
+ax.plot(z_array/20, gas_dens_N2, linestyle='--', color='#7BC618', label="$n_{gas} (N2)$")
 
 # Test plots
 #ax.plot(ts.t*c*1e3, e_dens_tot_test, color='red', label="Test")
