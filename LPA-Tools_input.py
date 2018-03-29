9d7fe1f5ba7a21279c5843942c9d417540c83700
Sebastian Mahncke <sebastian.mahncke@desy.de>
2018-03-29 17:33:33 +0200

    Update LPA-Tools_input.py

--- LPA-Tools_input.py
+++ LPA-Tools_input.py
@@ -15,20 +15,32 @@ from particles import particle_data as ptcl
 # LASER PARAMETER
 #----------------
 
+#Peak a0 of  the laser pulse
 a0 = 1.5
+
+#Wavelength of the laser (in microns)
 lambda_0 = 800e-3
+
+#Waist of the laser in the focus (in microns)
 w0 = 22
+
+#Laser duration (in femtoseconds)
 ctau = 10.1
+
+#Laser focus (in microns)
 zf = 50
-r = 0
-t = 0
 
 #-------------------
 # PARTICLE PARAMETER
 #-------------------
 
+#The used element (use the shortcut)
 element = 'N'
+
+#The energy level that should get ionized
 ion_level = 7
+
+#Energy of the ionized electrons after ionization (in MeV)
 energy = 0
 
 
@@ -36,29 +48,43 @@ energy = 0
 # SIMULATION PARAMETER
 #---------------------
 
+#Array over the laser pulse
 zz = np.linspace(3*ctau,-3*ctau,1e6)
 
+###################################################
 
 
-#--------
-# RESULTS
-#--------
+#------------------------------------
+# Calculate the ionization parameters
+#------------------------------------
 
 #Ionization energy
 U_i = ptcl.get_ion_energy(element,ion_level)
 
 #Ionization probability
-prob = ion.ionization_probability(U_i,energy,a0,zz,r,t,w0,ctau,zf,lambda_0)
+prob = ion.ionization_probability(U_i,energy,a0,zz,w0,ctau,zf,lambda_0)
 
 #Ionization degree
 degree = ion.ionization_degree(prob,zz)
 
 #Laser field
-laser_field = las.gaussian_field(a0,zz,r,t,w0,ctau,zf,lambda_0)
+laser_field = las.gaussian_field(a0,zz,w0,ctau,zf,lambda_0)
 
 #Laser envelope
 laser_envelope = las.gaussian_envelope(a0,zz,ctau)
 
+
+#-------------------
+# Prints the results
+#-------------------
+
+print("Results of the LPA-Tools ionization calculation script:")
+print("")
+print("The "+str(ion_level)+". niveau of the element '"+str(element)+"' has an ionization energy of "+str(U_i)+" MeV."
+print("")
+print("With a peak a0 of "+str(a0)+", the max. ionization probability is "+str(prob.max())+" %.")
+print("")
+print("The final degree of ionization behind the laser pulse is "+str(degree[len(degree)-1])+" %.")
 
 
 
