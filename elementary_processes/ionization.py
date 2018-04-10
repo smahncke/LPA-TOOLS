13d9268aec0aa6da527f53987166521e1cde92ce
Sebastian Mahncke <sebastian.mahncke@desy.de>
2018-04-10 19:42:17 +0200

    Added new function: get_min_a0
    New function added that returns the lowest needed value of a0 for ionization

--- elementary_processes/ionization.py
+++ elementary_processes/ionization.py
@@ -182,3 +182,39 @@ def ionization_energy_distribution(z,max_a,w0,ctau,zf,lambda_0,U_i,energy_range 
 		#Return the original function
 		return(energy_space,final_degree)
 
+
+def get_min_a0(z,w0,ctau,zf,lambda_0,U_i,energy,max_a = 10,ion_thres = 0.01):
+
+	"""
+	Calculates the lowest value of a0 that is needed for ionization
+
+	-INPUT:	-z [array]:				Array over the laser pulse
+			-w0 [float]:			Waist of the laser in the focus (in microns)
+			-ctau [float]:			Laser duration (in femtoseconds)
+			-zf [float]:			Laser focus (in microns)
+			-lambda_0 [float]:		Laser wavelength (in microns)
+			-U_i [float]:			Ionization energy (in eV)
+			-energy [float]:		Final energy of the ionized electrons (in J)
+			-max_a [float,opt]:		The upper limit for the laser peak a0 
+			-ion_thres [float,opt]:	The threshold/ minimum ionization degree
+
+	-RETURN: -min_a0 [float]:		The lower limit of a0 for ionization
+			
+	"""
+	#Calculate the ionization probability
+	prob = ionization_probability(z,max_a,w0,ctau,zf,lambda_0,U_i,energy)
+
+	#Calculate the ionization degree
+	degree = ionization_degree(z,prob)
+
+	#Get the laser envelope
+	envelope = las.gaussian_envelope(z,max_a,ctau)
+
+	#Check for which value of a0 the ionization degree is higher than the threshold
+	for i in range(0,len(degree)):
+		if degree[i] > ion_thres:
+			min_a0 = envelope[i]
+			break
+	
+	return(min_a0)	
+
