6dbac44e0cf9c393eadadceb7cc84a06300ebf1b
Sebastian Mahncke <sebastian.mahncke@desy.de>
2018-07-17 14:30:41 +0200

    Bugfix
    Bugfix in the electrical field of the gaussian laser pulse

--- laser/laser_profiles.py
+++ laser/laser_profiles.py
@@ -11,7 +11,7 @@ import scipy.integrate as integrate
 # Electrical field of the laser
 #------------------------------
 
-def gaussian_field(z, max_a, w0, ctau, zf, lambda_0, r = 0, t = 0):
+def gaussian_field_OLD(z, max_a, w0, ctau, zf, lambda_0, r = 0, t = 0):
 
 	"""
 	Calculates the electrical field of the gaussian laser pulse
@@ -68,3 +68,51 @@ def gaussian_envelope(z, max_a, ctau):
 	rms = ctau/np.sqrt(2)
 	
 	return max_a*np.exp(-(z-0)**2/(2*rms**2))
+
+#-------------------
+# Gaussian Field NEW
+#-------------------
+
+def gaussian_field(z,a0,lambda_0,w0,ctau,zf,x=0, y=0, t=0,theta_pol=0, phi2_chirp=0,cep_phase=0,z0=0):
+    """
+        See the docstring of LaserProfile.E_field
+        """
+    # Note: this formula is expressed with complex numbers for compactness
+    # and simplicity, but only the real part is used in the end
+    # (see final return statement)
+    # The formula for the laser (in complex numbers) is obtained by
+    # multiplying the Fourier transform of the laser at focus
+    # E(k_x,k_y,\omega) = exp( -(\omega-\omega_0)^2(\tau^2/4 + \phi^(2)/2)
+    # - (k_x^2 + k_y^2)w_0^2/4 ) by the paraxial propagator
+    # e^(-i(\omega/c - (k_x^2 +k_y^2)/2k0)(z-z_foc))
+    # and then by taking the inverse Fourier transform in x, y, and t
+    
+    # Diffraction and stretch_factor
+    
+    k0 = 2*np.pi/lambda_0
+    E0 = a0*const.m_e*const.c**2*k0/const.e
+    zr = 0.5*k0*w0**2
+
+    inv_zr = 1./zr
+    E0x = E0 * np.cos(theta_pol)
+    E0y = E0 * np.sin(theta_pol)
+    inv_ctau2 = 1./(ctau)**2
+    
+    prop_dir = 1
+    diffract_factor = 1. + 1j * prop_dir*(z - zf) * inv_zr
+    stretch_factor = 1 - 2j * phi2_chirp * const.c**2 * inv_ctau2
+    
+    # Calculate the argument of the complex exponential
+    exp_argument = - 1j*cep_phase \
+        + 1j*k0*( prop_dir*(z - z0) - const.c*t ) \
+        - (x**2 + y**2) / (w0**2 * diffract_factor) \
+        - 1./stretch_factor*inv_ctau2 * \
+            ( prop_dir*(z - z0) - const.c*t )**2
+    # Get the transverse profile
+    profile = np.exp(exp_argument) /(diffract_factor * stretch_factor**0.5)
+    
+    # Get the projection along x and y, with the correct polarization
+    Ex = E0x * profile
+    Ey = E0y * profile
+    
+    return( Ex.real)
